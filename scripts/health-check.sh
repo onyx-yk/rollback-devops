4a4fae3f264a6b0e3c2b53ff6be5289a2b7c3776
onyx-yk <thinkingcustard@gmail.com>
2025-08-23 21:04:31 +0530

    edited sh v3

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 # health-check.sh
 set -x
-echo "Checking if app is healthy..."
-
-for i in {1..12}; do  # try for 60 seconds
-    if curl -f http://localhost:8080; then
-        echo "App is healthy."
-        break
-    else
-        echo "Waiting."
-        sleep 5
-    fi
-done#!/bin/bash
-# health-check.sh
-set -x
 
 echo "Checking if app is healthy..."
 
